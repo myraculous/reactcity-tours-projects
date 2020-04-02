d705f8384d01c5eaa8ef4968a0496066d9996ad0
Miraculous-V <dusworld@gmail.com>
2020-04-02 13:15:07 +0100

    first commit

---
+++ src/tourData.js
@@ -0,0 +1,59 @@
+export const tourData = [
+    {
+      id: 1,
+      city: "new york",
+      img: "./img/newyork.jpeg",
+      name: "new york bridge tour",
+      info:
+        "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tenetur, nam omnis error corrupti eum assumenda enim odit architecto corporis. Sequi"
+    },
+    {
+      id: 2,
+      city: "paris",
+      img: "./img/paris.jpeg",
+      name: "paris lights tour",
+      info:
+        "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tenetur, nam omnis error corrupti eum assumenda enim odit architecto corporis. Sequi"
+    },
+    {
+      id: 3,
+      city: "london",
+      img: "./img/london.jpeg",
+      name: "london royal palace tour",
+      info:
+        "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tenetur, nam omnis error corrupti eum assumenda enim odit architecto corporis. Sequi"
+    },
+    {
+      id: 4,
+      city: "tokyo",
+      img: "./img/tokyo.jpeg",
+      name: "tokyo sushi tour",
+      info:
+        "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tenetur, nam omnis error corrupti eum assumenda enim odit architecto corporis. Sequi"
+    },
+    {
+        id: 5,
+        city: "abuja",
+        img: "./img/abuja.jpg",
+        name: "abuja sushi tour",
+        info:
+          "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tenetur, nam omnis error corrupti eum assumenda enim odit architecto corporis. Sequi"
+      },
+      {
+        id: 6,
+        city: "lagos",
+        img: "./img/lagos.jpg",
+        name: "lagos sushi tour",
+        info:
+          "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tenetur, nam omnis error corrupti eum assumenda enim odit architecto corporis. Sequi"
+      },
+      {
+        id: 7,
+        city: "port harcourt",
+        img: "./img/portharcourt.jpg",
+        name: "portharcourt sushi tour",
+        info:
+          "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tenetur, nam omnis error corrupti eum assumenda enim odit architecto corporis. Sequi"
+      }
+  ];
+  
